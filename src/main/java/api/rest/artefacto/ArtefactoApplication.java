60b49c854cfefe33b6ccb4334e27ce20e676c09f
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2026-07-07 12:52:03 -0500

    proyecto inicial

---
+++ src/main/java/api/rest/artefacto/ArtefactoApplication.java
@@ -0,0 +1,13 @@
+package api.rest.artefacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtefactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtefactoApplication.class, args);
+	}
+
+}
--- src/test/java/api/rest/artefacto/ArtefactoApplicationTests.java
+++ src/test/java/api/rest/artefacto/ArtefactoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.api;
+package api.rest.artefacto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class ArtefactoApplicationTests {
 
 	@Test
 	void contextLoads() {
